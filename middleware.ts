5756b03ea986eb3856ff779dfbfe4bfecf46b025
MikaeI <mikaelaast@gmail.com>
2024-10-06 19:40:49 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,13 @@ const USERNAME = process.env.BASIC_AUTH_USERNAME,
   PASSWORD = process.env.BASIC_AUTH_PASSWORD;
 
 export function middleware(request: NextRequest) {
+  if (!request.headers.get('authorization')) {
+    return new NextResponse(null, {
+      status: 401,
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+    });
+  }
+  
   const authHeader = request.headers.get('authorization'),
     base64Credentials = authHeader.split(' ')[1],
     credentials = Buffer.from(base64Credentials, 'base64').toString('ascii'),
