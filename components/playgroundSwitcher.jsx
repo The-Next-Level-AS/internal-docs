97506075b8a401065df86593afb2b33a832c87fe
Mikael Åsbjørnsson-Stensland <mikael@grensesnitt.no>
2024-10-25 00:36:32 +0200

    Misc

---
+++ components/playgroundSwitcher.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from "react";
+
+const PlaygroundSwitcher = () => {
+  const playgrounds = { A: useRef(null), B: useRef(null) },
+    [active, setActive] = useState("A"),
+    [streamlitReady, setStreamlitReady] = useState(false);
+
+  useEffect(() => {
+    const handleMessage = ({ data }) => {
+      if (typeof data === "string" && data.indexOf("nxtl_") === 0) {
+        const next =
+            active === "A" ? playgrounds.B.current : playgrounds.A.current,
+          nextSrc = new URL(next.src);
+
+        nextSrc.searchParams.set(
+          "log",
+          `${new URL(playgrounds[active].current.src).searchParams.get(
+            "log"
+          )}\n${data.split("nxtl_")[1]}`
+        );
+        setActive(active === "A" ? "B" : "A");
+        setStreamlitReady(false);
+        next.src = nextSrc.toString();
+      } else if (typeof data === "string" && data === "streamlit_ready")
+        setStreamlitReady(true);
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => window.removeEventListener("message", handleMessage);
+  });
+
+  useEffect(() => {
+    const curr = playgrounds[active === "A" ? "B" : "A"].current,
+      next = playgrounds[active].current;
+
+    if (streamlitReady) {
+      curr.style.opacity = "0";
+      curr.style.pointerEvents = "none";
+      next.style.opacity = "1";
+      next.style.pointerEvents = "initial";
+    }
+  }, [active, streamlitReady]);
+
+  return (
+    <>
+      <iframe
+        src="https://playground-47d69.ondigitalocean.app?embed=true&embed_options=hide_loading_screen&embed_options=disable_scrolling&log="
+        width="100%"
+        style={{
+          height: "1600px",
+          filter: "brightness(0.96875)",
+          borderRadius: "8px",
+          opacity: "0",
+        }}
+        ref={playgrounds.A}
+      />
+      <iframe
+        src="https://playground-47d69.ondigitalocean.app?embed=true&embed_options=hide_loading_screen&embed_options=disable_scrolling&log="
+        width="100%"
+        style={{
+          height: "1600px",
+          marginTop: "-1600px",
+          filter: "brightness(0.96875) sepia()",
+          borderRadius: "8px",
+          opacity: "0",
+        }}
+        ref={playgrounds.B}
+      />
+    </>
+  );
+};
+
+export default PlaygroundSwitcher;
