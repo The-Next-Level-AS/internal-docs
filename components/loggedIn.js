ce02dcfc22e509d8a7729e7b95e064bceedcb9d0
Mikael Åsbjørnsson-Stensland <mikael@grensesnitt.no>
2024-11-06 21:25:18 +0100

    Misc

--- components/loggedIn.js
+++ components/loggedIn.js
@@ -4,15 +4,7 @@ export default function LoggedIn() {
   const { data: session } = useSession();
 
   return (
-    <div
-      style={{
-        alignSelf: "start",
-        marginRight: "auto",
-        color: "hsl(215deg 25% 10% / 75%)",
-        fontWeight: "600",
-        textShadow: "rgba(255, 255, 255, 0.33) 1px 1px 1px",
-      }}
-    >
+    <div className="signedIn">
       Signed in as: {session.user.email}&nbsp;&nbsp;&nbsp;
       <button
         style={{
